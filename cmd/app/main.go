a9ee9e3895aa6f901a6c9575baa0dd986744d598
pacholoamit <pacholoamit.tech@gmail.com>
2022-08-03 09:16:53 +0800

    initial commit

---
+++ cmd/app/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Println(os.Getenv("INFO_FOOTTRAFFIK_OAUTH2_CLIENT_AUTH_PROVIDER"))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pacholoamit/google-analytics-activity-monitor
+
+go 1.19
+
+require github.com/joho/godotenv v1.4.0
