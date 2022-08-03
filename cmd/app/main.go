07d9c1c6aa3c4312f0bc06adedf1d7dd2fc2affb
pacholoamit <pacholoamit.tech@gmail.com>
2022-08-04 07:29:56 +0800

    implement go routines for browser & server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/pkg/browser"
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 type config struct {
@@ -21,6 +27,42 @@ type config struct {
 type application struct {
 	config config
 	oauth  *oauth2.Config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	cfg.setupConfig()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	oauth := createOAuthConfig(cfg)
+
+	app := &application{
+		config: cfg,
+		oauth:  oauth,
+		logger: logger,
+	}
+
+	go func() {
+		srv := &http.Server{
+			Addr:         fmt.Sprintf(":%d", 3000),
+			Handler:      app.routes(),
+			IdleTimeout:  time.Minute,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 30 * time.Second,
+		}
+		logger.Printf("starting server on %s", srv.Addr)
+		err := srv.ListenAndServe()
+		logger.Fatal(err)
+	}()
+	go func() {
+		url := app.oauth.AuthCodeURL("state") // For inclusing of refresh token
+
+		browser.OpenURL(url)
+	}()
+
 }
 
 func (c *config) setupConfig() {
@@ -29,19 +71,21 @@ func (c *config) setupConfig() {
 	viper.AddConfigPath(".")
 
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Enter you Client Id: ")
-		var clientId string
-		fmt.Scanln(&clientId)
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Print("Enter you Client Id: ")
+		clientId, _ := reader.ReadString('\n')
+		clientId = strings.TrimSpace(clientId)
 		viper.Set("client_id", clientId)
 
 		fmt.Println("Enter you Client Secret: ")
-		var clientSecret string
-		fmt.Scanln(&clientSecret)
+		clientSecret, _ := reader.ReadString('\n')
+		clientSecret = strings.TrimSpace(clientSecret)
 		viper.Set("client_secret", clientSecret)
 
 		fmt.Println("Enter you Redirect URL: ")
-		var redirectURL string
-		fmt.Scanln(&redirectURL)
+		redirectURL, _ := reader.ReadString('\n')
+		redirectURL = strings.TrimSpace(redirectURL)
 		viper.Set("redirect_url", redirectURL)
 
 		viper.SafeWriteConfig()
@@ -56,28 +100,20 @@ func (c *config) setupConfig() {
 	c.redirectURL = redirectURL
 }
 
-func main() {
-	var c config
+func createOAuthConfig(c config) *oauth2.Config {
+	oauth := &oauth2.Config{
+		ClientID:     c.clientId,
+		ClientSecret: c.clientSecret,
+		RedirectURL:  c.redirectURL,
+		Scopes: []string{
+			"https://www.googleapis.com/auth/business.manage",
+			"https://www.googleapis.com/auth/analytics.readonly",
+			"https://www.googleapis.com/auth/adwords",
+		},
+		Endpoint: google.Endpoint,
+	}
 
-	c.setupConfig()
-
-	fmt.Println(c.clientId)
-
-	// conf := &oauth2.Config{
-	// 	ClientID:     clientId,
-	// 	ClientSecret: clientSecret,
-	// 	RedirectURL:  redirectURL,
-	// 	Scopes: []string{
-	// 		"https://www.googleapis.com/auth/business.manage",
-	// 		"https://www.googleapis.com/auth/analytics.readonly",
-	// 		"https://www.googleapis.com/auth/adwords",
-	// 	},
-	// 	Endpoint: google.Endpoint,
-	// }
-
-	// url := conf.AuthCodeURL("state") // For inclusing of refresh token
-
-	// browser.OpenURL(url)
+	return oauth
 }
 
 func (app *application) routes() *httprouter.Router {
