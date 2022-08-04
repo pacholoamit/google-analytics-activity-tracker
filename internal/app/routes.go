98375dd05b2d9eef113035896658fee5f837bea0
pacholoamit <pacholoamit.tech@gmail.com>
2022-08-05 05:26:32 +0800

    Create struct for changeHistoryEventsRequest

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -48,26 +48,22 @@ func (app *Application) successHandler(w http.ResponseWriter, r *http.Request) {
 
 	accounts := app.ListAccounts(client)
 
-	for _, account := range accounts {
-		fmt.Println(account.Name)
-	}
-
 	app.GetChangeHistory(accounts, client)
 }
 
 func (app *Application) ListAccounts(c *http.Client) []accountModel {
 
-	resp, err := c.Get("https://analyticsadmin.googleapis.com/v1beta/accounts/?pageSize=200")
+	res, err := c.Get("https://analyticsadmin.googleapis.com/v1beta/accounts/?pageSize=200")
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	var responseJSON struct {
+	var response struct {
 		Accounts []accountModel `json:"accounts"`
 	}
 
-	err = json.NewDecoder(resp.Body).Decode(&responseJSON)
+	err = json.NewDecoder(res.Body).Decode(&response)
 
 	if err != nil {
 		panic(err)
@@ -75,7 +71,7 @@ func (app *Application) ListAccounts(c *http.Client) []accountModel {
 
 	var accountsArray []accountModel
 
-	accountsList := append(accountsArray, responseJSON.Accounts...)
+	accountsList := append(accountsArray, response.Accounts...)
 
 	return accountsList
 
@@ -98,30 +94,39 @@ func (app *Application) GetChangeHistory(acc []accountModel, c *http.Client) {
 		go func(account accountModel) {
 			url := fmt.Sprintf("https://analyticsadmin.googleapis.com/v1beta/%s:searchChangeHistoryEvents", account.Name)
 
-			postBody := []byte(`{
-  "earliestChangeTime": "2014-10-02T15:01:23Z",
-  "pageSize": 1000,
-  "resourceType": [
-    "ACCOUNT",
-    "PROPERTY",
-    "GOOGLE_ADS_LINK",
-    "GOOGLE_SIGNALS_SETTINGS",
-    "CONVERSION_EVENT",
-    "MEASUREMENT_PROTOCOL_SECRET",
-    "DATA_RETENTION_SETTINGS",
-    "DISPLAY_VIDEO_360_ADVERTISER_LINK",
-    "DISPLAY_VIDEO_360_ADVERTISER_LINK_PROPOSAL",
-    "DATA_STREAM",
-    "ATTRIBUTION_SETTINGS"
-  ]
-}`)
-			resp, err := c.Post(url, "application/json", bytes.NewBuffer(postBody))
+			type changeHistoryEventsRequest struct {
+				EarliestChangeTime string   `json:"earliestChangeTime"`
+				PageSize           int      `json:"pageSize"`
+				ResourceType       []string `json:"resourceType"`
+			}
+
+			postBody := &changeHistoryEventsRequest{
+				EarliestChangeTime: "2022-07-01T00:00:00.000Z",
+				PageSize:           100,
+				ResourceType: []string{
+					"ACCOUNT",
+					"PROPERTY",
+					"GOOGLE_ADS_LINK",
+					"GOOGLE_SIGNALS_SETTINGS",
+					"CONVERSION_EVENT",
+					"MEASUREMENT_PROTOCOL_SECRET",
+					"DATA_RETENTION_SETTINGS",
+					"DISPLAY_VIDEO_360_ADVERTISER_LINK",
+					"DISPLAY_VIDEO_360_ADVERTISER_LINK_PROPOSAL",
+					"DATA_STREAM",
+					"ATTRIBUTION_SETTINGS",
+				},
+			}
+
+			b, _ := json.Marshal(postBody)
+
+			res, err := c.Post(url, "application/json", bytes.NewBuffer(b))
 
 			if err != nil {
 				app.logger.Fatalln(err)
 			}
 
-			body, err := ioutil.ReadAll(resp.Body)
+			body, err := ioutil.ReadAll(res.Body)
 
 			fmt.Println(string(body))
 
